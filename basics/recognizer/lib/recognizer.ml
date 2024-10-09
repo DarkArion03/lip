e9918aeb0557dbdcb25564eddfe0a583cb472424
Docker <docker@example.com>
2024-10-09 15:40:00 +0100

    recognizer

--- basics/recognizer/lib/recognizer.ml
+++ basics/recognizer/lib/recognizer.ml
@@ -1,8 +1,31 @@
-let lang1 _ = failwith ""
+let lang1 l = match l with
+| [] -> false
+| _ -> List.fold_left (fun acc x -> acc && (x == 0 || x == 1)) true l;;
 
-let lang2 _ = failwith ""
+let rec alt_lang1 l = match l with
+| [] -> false
+| [x] -> (x == 0 || x == 1)
+| x::l' -> (x == 0 || x == 1) && lang1 l';;
 
-let lang3 _ = failwith ""
+let rec alt_alt_lang1 l = match l with
+| [] -> false
+| x::l' -> (x == 0 || x == 1) && (l' == [] || lang1 l');;
+
+let rec lang2 l = match l with 
+| [] -> true
+| x::l' -> match x with
+  | 0 -> List.fold_left (fun acc x -> acc && x == 1) true l'
+  | _ -> List.fold_left (fun acc x -> acc && x == 1) true l;;
+
+let rec last l = match l with 
+| [] -> failwith ""
+| [x] -> x
+| _::l' -> last l';;
+
+let rec lang3 l = match l with 
+| [] -> false
+| [_] -> false
+| x::l' -> x == 0 && List.fold_left (fun acc x -> acc && (x == 0 || x == 1)) true l' && last l' == 0;;
 
 let lang4 _ = failwith ""
 
