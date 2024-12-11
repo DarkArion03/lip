7662784929eaa91c3288469ce505219f28367db9
Docker <docker@example.com>
2024-12-11 14:02:49 +0000

    m

--- imp/while/lib/main.ml
+++ imp/while/lib/main.ml
@@ -1,16 +1,11 @@
 open Ast
 open Types
 
-(* Stato globale per memorizzare le variabili *)
-let state = Hashtbl.create 10 
+let bind (st : state) (id : ide) (v : exprval) : state =
+  fun x -> if x = id then v else st x
 
-(* Funzioni per aggiornare e ottenere lo stato delle variabili *)
-let update_state var value = 
-  Hashtbl.replace state var value
-
-let get_state var = 
-  try Hashtbl.find state var 
-  with Not_found -> raise (UnboundVar("Unbound variable: " ^ var))
+let bottom : state =
+  fun _ -> raise (UnboundVar "Access to uninitialized variable")
 
 (* Parsing dell'input e generazione del comando *)
 let parse (s : string) : conf =
@@ -18,80 +13,73 @@ let parse (s : string) : conf =
   let ast = Parser.prog Lexer.read lexbuf in
   Cmd(ast, fun _ -> Nat 0)  (* Restituisci una struttura conf che include cmd e uno stato iniziale *)
 
-(* Valutazione delle espressioni *)
-let rec eval_expr = function
+(* Evaluating expressions *)
+let rec eval_expr (st : state) = function
   | True -> Bool true
   | False -> Bool false
-  | Var(s) -> 
-    (try 
-       get_state s
-     with 
-       UnboundVar _ -> failwith "Used variables must be initialized")
-  | Const(i) -> Nat i
-  | Not(e) -> 
-    (match eval_expr e with
-      | Bool b1 -> Bool (not b1)
-      | _ -> failwith "Not argument must be a boolean value")
-  | And(e1, e2) -> 
-    (match (eval_expr e1, eval_expr e2) with
-      | (Bool b1, Bool b2) -> Bool (b1 && b2)
-      | _ -> failwith "And arguments must be boolean values")
-  | Or(e1, e2) -> 
-    (match (eval_expr e1, eval_expr e2) with
-      | (Bool b1, Bool b2) -> Bool (b1 || b2)
-      | _ -> failwith "Or arguments must be boolean values")
-  | Add(e1, e2) -> 
-    (match (eval_expr e1, eval_expr e2) with
-      | (Nat n1, Nat n2) -> Nat (n1 + n2)
-      | _ -> failwith "Add arguments must be natural values")
-  | Sub(e1, e2) -> 
-    (match (eval_expr e1, eval_expr e2) with
-      | (Nat n1, Nat n2) -> Nat (n1 - n2)
-      | _ -> failwith "Sub arguments must be natural values")
-  | Mul(e1, e2) -> 
-    (match (eval_expr e1, eval_expr e2) with
-      | (Nat n1, Nat n2) -> Nat (n1 * n2)
-      | _ -> failwith "Mul arguments must be natural values")
-  | Eq(e1, e2) -> 
-    (match (eval_expr e1, eval_expr e2) with
-      | (Bool b1, Bool b2) -> Bool (b1 = b2)
-      | (Nat n1, Nat n2) -> Bool (n1 = n2)
-      | _ -> failwith "Eq arguments must be boolean or natural values")
-  | Leq(e1, e2) -> 
-    (match (eval_expr e1, eval_expr e2) with
-      | (Nat n1, Nat n2) -> Bool (n1 <= n2)
-      | _ -> failwith "Leq arguments must be natural values")
+  | Var id -> 
+    (try st id with Not_found -> raise (UnboundVar id))
+  | Const i -> Nat i
+  | Not e -> 
+    (match eval_expr st e with
+     | Bool b -> Bool (not b)
+     | _ -> failwith "Not expects a boolean")
+  | And (e1, e2) -> 
+    (match (eval_expr st e1, eval_expr st e2) with
+     | (Bool b1, Bool b2) -> Bool (b1 && b2)
+     | _ -> failwith "And expects booleans")
+  | Or (e1, e2) -> 
+    (match (eval_expr st e1, eval_expr st e2) with
+     | (Bool b1, Bool b2) -> Bool (b1 || b2)
+     | _ -> failwith "Or expects booleans")
+  | Add (e1, e2) -> 
+    (match (eval_expr st e1, eval_expr st e2) with
+     | (Nat n1, Nat n2) -> Nat (n1 + n2)
+     | _ -> failwith "Add expects naturals")
+  | Sub (e1, e2) -> 
+    (match (eval_expr st e1, eval_expr st e2) with
+     | (Nat n1, Nat n2) -> Nat (n1 - n2)
+     | _ -> failwith "Sub expects naturals")
+  | Mul (e1, e2) -> 
+    (match (eval_expr st e1, eval_expr st e2) with
+     | (Nat n1, Nat n2) -> Nat (n1 * n2)
+     | _ -> failwith "Mul expects naturals")
+  | Eq (e1, e2) -> 
+    (match (eval_expr st e1, eval_expr st e2) with
+     | (Nat n1, Nat n2) -> Bool (n1 = n2)
+     | _ -> failwith "Eq expects naturals")
+  | Leq (e1, e2) -> 
+    (match (eval_expr st e1, eval_expr st e2) with
+     | (Nat n1, Nat n2) -> Bool (n1 <= n2)
+     | _ -> failwith "Leq expects naturals")
 
-(* Tracciamento delle riduzioni di un comando *)
+(* Reducing commands (small-step semantics) *)
 let rec trace1 = function
-  | Not(True) -> False
-  | Not(False) -> True
-  | Not(e1) -> Not(trace1 e1)
-  | And(True, e2) -> e2
-  | And(False, _) -> False
-  | And(e1, e2) -> And(trace1 e1, e2)
-  | Or(True, _) -> True
-  | Or(False, e2) -> e2
-  | Or(e1, e2) -> Or(trace1 e1, e2) 
-  | Add(Const n1, Const n2) -> Const (n1 + n2)
-  | Add(e1, e2) -> Add(trace1 e1, e2)
-  | Sub(Const n1, Const n2) -> Const (n1 - n2)
-  | Sub(e1, e2) -> Sub(trace1 e1, e2)
-  | Mul(Const n1, Const n2) -> Const (n1 * n2)
-  | Mul(e1, e2) -> Mul(trace1 e1, e2)
-  | Eq(Const n1, Const n2) -> if n1 = n2 then True else False
-  | Eq(e1, e2) -> Eq(trace1 e1, e2)
-  | Leq(Const n1, Const n2) -> if n1 <= n2 then True else False
-  | Leq(e1, e2) -> Leq(trace1 e1, e2)
+  | Cmd (Skip, st) -> St st
+  | Cmd (Assign (x, e), st) -> 
+    let v = eval_expr st e in
+    St (bind st x v)
+  | Cmd (Seq (c1, c2), st) -> 
+    (match trace1 (Cmd (c1, st)) with
+     | St st' -> Cmd (c2, st')
+     | Cmd (c1', st') -> Cmd (Seq (c1', c2), st'))
+  | Cmd (If (e, c1, c2), st) -> 
+    (match eval_expr st e with
+     | Bool true -> Cmd (c1, st)
+     | Bool false -> Cmd (c2, st)
+     | _ -> failwith "If condition must be boolean")
+  | Cmd (While (e, c), st) -> 
+    (match eval_expr st e with
+     | Bool true -> Cmd (Seq (c, While (e, c)), st)
+     | Bool false -> St st
+     | _ -> failwith "While condition must be boolean")
   | _ -> raise NoRuleApplies
 
-
-
-(* Tracciamento dell'esecuzione per un numero di passi *)
-let rec trace n e = 
-  if n <= 0 then [e]  
+(* Executing n steps *)
+let rec trace n conf = 
+  if n <= 0 then [conf]
   else 
-    try
-      let e' = trace1 e
-      in e::(trace (n-1) e')
-    with NoRuleApplies -> [e]
+    try 
+      let conf' = trace1 conf in
+      conf :: trace (n - 1) conf'
+    with NoRuleApplies -> [conf]
